1c2d0871ff6c7dbff736ffaf7954c07b0cff70c7
Bank Tanapat <tanapat_shiny-spirit@hotmail.com>
2016-09-01 23:06:29 +0700

    Authentication Pages

---
+++ src/app/core/core.module.js
@@ -0,0 +1,20 @@
+(function ()
+{
+    'use strict';
+
+    angular
+        .module('app.core',
+            [
+                'ngAnimate',
+                'ngAria',
+                'ngCookies',
+                'ngMessages',
+                'ngResource',
+                'ngSanitize',
+                'ngMaterial',
+				'textAngular',
+				'xeditable',
+                'pascalprecht.translate',
+                'ui.router'
+            ]);
+})();
---
+++ src/app/index.module.js
@@ -0,0 +1,33 @@
+(function ()
+{
+    'use strict';
+
+    /**
+     * Main module of the Fuse
+     */
+    angular
+        .module('fuse', [
+			// Common 3rd Party Dependencies
+            
+            'textAngular',
+            'xeditable',
+
+            // Core
+            'app.core',
+
+            // Navigation
+            'app.navigation',
+
+            // Toolbar
+            'app.toolbar',
+
+            // Quick Panel
+            'app.quick-panel',
+			
+			
+			//Pages
+			'app.pages',
+            // Sample
+            'app.sample'
+        ]);
+})();
